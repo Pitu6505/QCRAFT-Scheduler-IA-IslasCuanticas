ba80fa247011767bc5533608a093921235f32a41
Pitu6505 <asanchezmu@aumnos.unex.es>
2025-12-17 10:06:04 +0100

    Subida de documentacion

--- config.py
+++ config.py
@@ -6,12 +6,12 @@ FORCE_THRESHOLD = 12  # Umbral de iteraciones para forzar la prioridad
 NUM_ENTRENAMIENTO = 30
 
 # Distancia mínima entre circuitos (número de nodos)
-MIN_CIRCUIT_DISTANCE = 2
+MIN_CIRCUIT_DISTANCE = 0
 
 # Umbral máximo aceptable de ruido/temperatura para usar un nodo
 MAX_NOISE_THRESHOLD = 312.15 #0.05575   # Ajustado para máquinas no-Brisbane (era 312.0550)
 
-Porcentaje_util = 70  # Porcentaje de qubits a utilizar según el umbral dinámico, valor entre 0 y 100
+Porcentaje_util = 95  # Porcentaje de qubits a utilizar según el umbral dinámico, valor entre 0 y 100
 
 #Configuracion de la particiones del grafo 
 USE_PARTITION = False       # True para activar particionado
