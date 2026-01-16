e1766fdc502c1d2019adeb1c23ac8a9e6c0f8a5c
Pitu6505 <asanchezmu@aumnos.unex.es>
2026-01-16 12:19:48 +0100

    Cambio en el circuito

--- circuits-code/IBM/Grover/Grover1.py
+++ circuits-code/IBM/Grover/Grover1.py
@@ -1,23 +1,23 @@
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
 import numpy as np
 
-qreg = QuantumRegister(2, 'q')
-creg = ClassicalRegister(2, 'meas')
+qreg_q = QuantumRegister(2, 'q')
+creg_meas = ClassicalRegister(2, 'meas')
 
-circuit = QuantumCircuit(qreg, creg)
+circuit = QuantumCircuit(qreg_q, creg_meas)
 
 
-circuit.h(qreg[0])
-circuit.h(qreg[1])
+circuit.h(qreg_q[0])
+circuit.h(qreg_q[1])
 
-circuit.x(qreg[1])
-circuit.h(qreg[1])
+circuit.x(qreg_q[1])
+circuit.h(qreg_q[1])
 
-circuit.cx(qreg[0], qreg[1])
+circuit.cx(qreg_q[0], qreg_q[1])
 
-circuit.h(qreg[1])
-circuit.x(qreg[1])
-circuit.h(qreg[1])
+circuit.h(qreg_q[1])
+circuit.x(qreg_q[1])
+circuit.h(qreg_q[1])
 
-circuit.measure(qreg[0], creg[0])
-circuit.measure(qreg[1], creg[1])
+circuit.measure(qreg_q[0], creg_meas[0])
+circuit.measure(qreg_q[1], creg_meas[1])
